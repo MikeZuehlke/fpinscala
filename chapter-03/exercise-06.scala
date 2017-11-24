984ed0cd2f9fbd66d1b279191abdfb43b5d347b0
Mike Zühlke <mike.zuehlke@awin.com>
2017-11-24 20:10:13 +0100

    Create exercise-06.scala

---
+++ chapter-03/exercise-06.scala
@@ -0,0 +1,20 @@
+/**
+ * Write a function which removes the last element
+ * from a list.
+ */
+object Exercise0306 {
+
+  def init[A](as: List[A]): List[A] = {
+    as match {
+      case Nil      => Nil
+      case h :: Nil => Nil
+      case h :: t   => h :: init(t)
+    }
+  }
+   
+  def main(args: Array[String]) = {
+    println(init(List())           == Nil)
+    println(init(List(1))          == Nil)
+    println(init(List(1, 2, 3, 4)) == List(1,2,3))
+  }
+}
